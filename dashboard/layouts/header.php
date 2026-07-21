7cd331afda286580ddfbba1833b12c9693287dce
Your Name <hatheesha6504@gmail.com>
2026-07-21 12:44:58 +0530

    feat: add reusable dashboard layout header with session management and user status synchronization

--- dashboard/layouts/header.php
+++ dashboard/layouts/header.php
@@ -72,7 +72,7 @@ $current = basename($_SERVER['PHP_SELF']);
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Lumus Studio Dashboard</title>
+    <title>Lumos Studio Dashboard</title>
     <link rel="icon" type="image/x-icon" href="../../uploads/lumos.jpg">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
